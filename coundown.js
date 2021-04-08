2eab21f269f605e688df74550ec992898fc823f3
225141125 <80028798+225141125@users.noreply.github.com>
2021-04-08 17:09:00 +0530

    Add files via upload

---
+++ coundown.js
@@ -0,0 +1,41 @@
+
+let i=10;
+let text=document.getElementById('count');
+
+
+setTimeout(() =>{
+    text.innerHTML=i;
+    setTimeout(() =>{
+        text.innerHTML=i--;
+        setTimeout(() =>{
+            text.innerHTML=i--;
+            setTimeout(() =>{
+                text.innerHTML=i--;
+                setTimeout(() =>{
+                   text.innerHTML=i--
+                    setTimeout(() =>{
+                       text.innerHTML=i--;
+                        setTimeout(() =>{
+                            text.innerHTML=i--;
+                            setTimeout(() =>{
+                                text.innerHTML=i--;
+                                setTimeout(() =>{
+                                    text.innerHTML=i--;
+                                    setTimeout(() =>{
+                                       text.innerHTML=i--;
+                                        setTimeout(() =>{
+                                            text.innerHTML="HAPPY NEW YEAR";
+                                            text.style.fontSize="8em";
+                                            text.style.textAlign="center";
+                                            text.style.color="yellow";
+                                        },1000);
+                                    },1000);
+                                },1000);
+                            },1000);
+                        },1000);
+                    },1000);
+                },1000);
+            },1000);
+        },1000); 
+    },1000);
+},1000);
